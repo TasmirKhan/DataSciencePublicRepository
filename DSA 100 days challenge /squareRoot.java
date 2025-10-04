5e036248a888bb06df5103bcf59d6e491e2bfc88
Tasmir Khan <masteroffcodes@gmail.com>
2025-10-04 17:09:46 +0000

    square root of a number using two pointer binary search approach

--- DSA 100 days challenge /squareRoot.java
+++ DSA 100 days challenge /squareRoot.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class squareRoot {
-    
+    public static void main(String[] args) {
+        System.out.println("Enter the number you want to find the squareRoot of ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int low =0, high =x;
+        while(low<high){
+             int mid = (low+high)/2;
+             int sq = mid*mid;
+             if(sq==x){ System.err.println(mid); break;}
+             else if(sq>x) high = mid-1;
+             else low = mid+1;
+        }
+       System.err.println(low);
+    }
 }
